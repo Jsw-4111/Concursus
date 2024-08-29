06297dbad8f784050c1f2202a01511fe303c28d2
JW <16844695+Jsw-4111@users.noreply.github.com>
2024-08-28 19:16:37 -0500

    Moves navigator input logic to the CharacterInputHandler for separation of concerns

--- src/Character/Navigation/Navigator.gd
+++ src/Character/Navigation/Navigator.gd
@@ -5,17 +5,14 @@ class_name Navigator
 @onready var playerCamera := $"../../Camera3D" as Camera3D
 
 signal terrain_Clicked(pos: Vector3)
-func _input(event: InputEvent) -> void:
-	if (event is InputEventMouseButton and 
-		event.button_index == MOUSE_BUTTON_RIGHT and 
-		event.pressed):
-		var pointStart = playerCamera.project_ray_origin(event.position)
-		var pointEnd = pointStart + playerCamera.project_ray_normal(event.position) * 1000.0
-		
-		var nearestPointOnMesh := NavigationServer3D.map_get_closest_point_to_segment(
-			get_world_3d().navigation_map,
-			pointStart,
-			pointEnd
-		)
-		
-		terrain_Clicked.emit(nearestPointOnMesh)
+func navigateTo(clickedPosition: Vector2):
+	var pointStart = playerCamera.project_ray_origin(clickedPosition)
+	var pointEnd = pointStart + playerCamera.project_ray_normal(clickedPosition) * 1000.0
+	
+	var nearestPointOnMesh := NavigationServer3D.map_get_closest_point_to_segment(
+		get_world_3d().navigation_map,
+		pointStart,
+		pointEnd
+	)
+	
+	terrain_Clicked.emit(nearestPointOnMesh)
